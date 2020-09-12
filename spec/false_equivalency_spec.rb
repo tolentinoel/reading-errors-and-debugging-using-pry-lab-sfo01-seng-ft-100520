f89a17d954e75e423396fa12e56a5e992576c407
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-12 02:17:37 +0000

    Automatically backed up by Learn

--- spec/false_equivalency_spec.rb
+++ spec/false_equivalency_spec.rb
@@ -1,8 +1,8 @@
-require_relative '../fix_using_tests/false_equivalency.rb'
+require_relative '../fix_using_tests/false_equivalency'
 
 describe "false_equivalency" do
   it "`selection` returns the correct string based on user input" do
-    expect(selection(1)).to be_an(Integer) eq("YUM YUM MUNCH MUNCH MUNCH")
-    expect(selection(2)).to be_an(Integer) eq("HAM HAM HAM IN MY TUMMY")
+    expect(selection(1)).to  eq("YUM YUM MUNCH MUNCH MUNCH")
+    expect(selection(2)).to  eq("HAM HAM HAM IN MY TUMMY")
   end
 end
